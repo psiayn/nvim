1d929bb4e14ecde5720661b5cc5e5668bbcf4264
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2024-03-20 22:15:11 +0530

    colorscheme chaaange

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
   {
-    "zootedb0t/citruszest.nvim",
+    "nyoom-engineering/oxocarbon.nvim",
     lazy = false,
     priority = 1000,
     config = function()
+      vim.opt.background = "dark"
 	    vim.cmd([[
-	  if has('termguicolors')
-			set termguicolors
-		endif
-
-		colorscheme citruszest
+          if has('termguicolors')
+            set termguicolors
+          endif
+          colorscheme oxocarbon
 	    ]])
     end
   }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,20 @@
 return {
   {"b3nj5m1n/kommentary"},
+  {
+    "TobinPalmer/Tip.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "rcarriga/nvim-notify"
+    },
+    event = "VimEnter",
+    init = function()
+      -- Default config
+      --- @type Tip.config
+      require("tip").setup({
+        seconds = 2,
+        title = "Tip!",
+        url = "https://vimiscool.tech/neotip", -- Or https://vimiscool.tech/neotip
+      })
+    end,
+  }
 }
