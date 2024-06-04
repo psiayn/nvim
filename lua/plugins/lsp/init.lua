5e7f0d3cd37bfa8177a777d5aa1eae2c8bd66288
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2024-06-04 19:38:26 +0530

    fix: nvim treesitter crashes for svelte so using diff plugin

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -58,5 +58,10 @@ return {
     })
     end
   },
-
+  {
+    'leafOfTree/vim-svelte-plugin',
+    config = function()
+      vim.cmd([[let g:vim_svelte_plugin_load_full_syntax = 1]])
+    end
+  },
 }
